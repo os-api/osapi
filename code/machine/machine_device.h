e7c1c36724b98a02dbe2f557c2692d8c6967e997
Joao Costa <jmpc1111@gmail.com>
2022-01-09 13:10:15 +0000

    Add prototype for managing machine devices.

--- code/machine/machine_device.h
+++ code/machine/machine_device.h
@@ -3,12 +3,12 @@
 // File description:
 //
 // Author:	Joao Costa
-// Purpose:	Provide Hardware Module API
+// Purpose:	Provide machine devices related API
 //
 // *****************************************************************************************
 
-#ifndef OSAPI_MACHINE_H_
-#define OSAPI_MACHINE_H_
+#ifndef OSAPI_MACHINE_DEVICE_H_
+#define OSAPI_MACHINE_DEVICE_H_
 
 // Make sure that header is easily imported from c++
 #ifdef __cplusplus
@@ -25,15 +25,11 @@ extern "C" {
 // Generic OSAPI includes
 #include "general/general.h"
 #include "status/status_types.h"
+#include "common/common_list.h"
 
 // Own declarations
 #include "machine/machine_types.h"
-#include "machine/machine_ip.h"
-#include "machine/machine_host.h"
-#include "machine/machine_domain.h"
-#include "machine/machine_cpu.h"
-#include "machine/machine_device.h"
-#include "machine/machine_virtual.h"
+
 
 /// *****************************************************************************************
 ///
@@ -55,9 +51,17 @@ extern "C" {
 
 #pragma GCC visibility push(default)		// Start of public interface
 
-/// @brief Declares if the module is supported on the current implementation
-/// @return SUCCESS or FAILURE
-t_status machine_module_supported( void	);
+
+/// @brief Obtain the number of machine devices
+/// @param [out] number - The count of devices reported by the OS
+/// @return Operation status
+t_status machine_device_getNumber( t_size * number );
+
+/// @brief Get a list of machine devices
+/// @param [out] devList - Device List
+/// @return Operation status
+t_status machine_device_getList( t_list * devList );
+
 
 
 #pragma GCC visibility pop			// End of public interface
@@ -70,4 +74,4 @@ t_status machine_module_supported( void	);
 }
 #endif
 
-#endif /* OSAPI_MACHINE_H_ */
+#endif // OSAPI_MACHINE_DEVICE_H_
